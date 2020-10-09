1bf139eb5a923565df3c9d8b914aac0146f35cce
Andres Fernando <andres.fernando08@gmail.com>
2020-10-08 23:27:31 -0500

    [mod] constraint for a field in vehicles model

--- server/models/vehicles.js
+++ server/models/vehicles.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 let vehicleSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+    name: { type: String, required: true, maxlength: 100 },
     label: { type: String, required: true },
     emitfactor: { type: String, required: true }
 }, { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } });
