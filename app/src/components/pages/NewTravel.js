c61384c7fc1bce0a166ae9acdb5a4c24cf52fd3c
Andres Fernando <andres.fernando08@gmail.com>
2020-10-08 23:29:46 -0500

    [add] newTravel form and travels list

---
+++ app/src/components/pages/NewTravel.js
@@ -0,0 +1,194 @@
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { Formik } from 'formik';
+import * as yup from 'yup';
+import { Button, Checkbox, Container, Form, Grid, Header, Message, Modal, Select } from 'semantic-ui-react';
+import * as actions from '../../store/actions';
+import SemanticField from '../../helpers/hoc/SemanticField';
+
+
+function NewTravel(props) {
+    const [error, setError] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const [modalPerson, setModalPerson] = useState(false)
+    const [worker, setWorker] = useState({
+        firstName: '',
+        lastName: ''
+    })
+
+    const formSubmittedHandler = values => {
+        console.log(values)
+    }
+
+    const modalPeopleHandler = e => {
+        e.preventDefault()
+        setModalPerson(!modalPerson)
+    }
+
+    useEffect(() => {
+        console.log('[NewTravel] loaded!')
+        props.onFetchVehicles()
+        initialWorkers.splice(0, initialWorkers.length) // reset workers list
+    }, [])
+
+    return (
+        <Container fluid>
+            <Grid columns={1}>
+                <Grid.Row centered>
+                    <Grid.Column width={6}>
+                        <Header as='h2' textAlign='center'>Registra un nuevo viaje</Header>
+                        <Formik 
+                            validationSchema={schemaValidation} 
+                            initialValues={{
+                                startPoint: '',
+                                finalPoint: '',
+                                vehicleId: '',
+                                kilometers: '',
+                                workers: initialWorkers,
+                                roundTrip: false
+                            }} 
+                            onSubmit={formSubmittedHandler}>
+                            {({
+                                handleSubmit, handleChange, handleBlur, setFieldValue,
+                                values, touched, isValid, errors
+                            }) => {
+                                return (
+                                    <Form 
+                                        error={error} 
+                                        loading={loading} 
+                                        onSubmit={handleSubmit}>
+                                        {
+                                            error 
+                                                ? <Message error header='Ocurrió un error inesperado' content={error} />
+                                                : null
+                                        }
+                                        <Form.Field>
+                                            Punto de inicio
+                                            <Form.Input 
+                                                name='startPoint' value={values.startPoint} onChange={handleChange}
+                                                error={touched.startPoint && errors.startPoint} autoComplete='off' />
+                                        </Form.Field>
+                                        <Form.Field>
+                                            Punto de termino
+                                            <Form.Input 
+                                                name='finalPoint' value={values.finalPoint} onChange={handleChange}
+                                                error={touched.finalPoint && errors.finalPoint} autoComplete='off' />
+                                        </Form.Field>
+                                        <Form.Field>
+                                            Medio de transporte
+                                            <Form.Select 
+                                                control={Select} options={
+                                                    props.vehicles && props.vehicles.map(item => 
+                                                        ({ key: item._id, value: item.label, text: item.name }))
+                                                } 
+                                                placeholder='Seleccione un vehiculo' 
+                                                name='vehicleId' value={values.vehicleId} onChange={(e, { value }) => setFieldValue('vehicleId', value)}
+                                                error={touched.vehicleId && errors.vehicleId} />
+                                        </Form.Field>
+                                        <Form.Field>
+                                            Kilometros recorridos
+                                            <Form.Input 
+                                                name='kilometers' value={values.kilometers} onChange={handleChange}
+                                                error={touched.kilometers && errors.kilometers} autoComplete='off' />
+                                        </Form.Field>
+                                        <Form.Field>
+                                            <SemanticField
+                                                name='roundTrip'
+                                                label='¿Ida y vuelta?'
+                                                onChange={handleChange}
+                                                checked={values.roundTrip}
+                                                WrappedCmp={Checkbox} />
+                                        </Form.Field>
+                                        <Container textAlign='center'>
+                                            <Modal 
+                                                open={modalPerson} 
+                                                trigger={<Button size='large' onClick={modalPeopleHandler}>Añadir trabajadores</Button>}
+                                                onClose={() => setModalPerson(!modalPerson)} size='tiny'>
+                                                <Modal.Header>Añadir trabajadores al viaje</Modal.Header>
+                                                <Modal.Content>
+                                                    <Form.Field>
+                                                        Nombre
+                                                        <Form.Input 
+                                                            name='firstNameWorker' value={worker.firstName} onChange={(e) => setWorker({
+                                                                ...worker,
+                                                                firstName: e.target.value
+                                                            })} autoComplete='off' />
+                                                    </Form.Field>
+                                                    <Form.Field>
+                                                        Apellido
+                                                        <Form.Input 
+                                                            name='lastNameWorker' value={worker.lastName} onChange={(e) => setWorker({
+                                                                ...worker,
+                                                                lastName: e.target.value
+                                                            })} autoComplete='off' />
+                                                    </Form.Field>
+                                                </Modal.Content>
+                                                <Modal.Actions>
+                                                    <Button 
+                                                        content='Guardar' labelPosition='right' 
+                                                        onClick={() => {
+                                                            if (worker.firstName && worker.lastName) {
+                                                                initialWorkers.push({
+                                                                    firstName: worker.firstName,
+                                                                    lastName: worker.lastName
+                                                                })
+                                                                setFieldValue('workers', initialWorkers)
+                                                                setWorker({
+                                                                    firstName: '',
+                                                                    lastName: ''
+                                                                })
+                                                            }
+                                                            setModalPerson(!modalPerson)
+                                                        }}
+                                                        icon='checkmark' positive />
+                                                </Modal.Actions>
+                                            </Modal>
+                                            <Button primary size='large' type='submit'>Registrar</Button>
+                                        </Container>
+                                    </Form>
+                                )
+                            }}
+                        </Formik>
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row centered>
+                    <Grid.Column width={6}>
+                        {initialWorkers.length > 0 ? <h4>Trabajadores añadidos:</h4> : null}
+                        <ul>
+                        {initialWorkers && initialWorkers.map((item, i) => (
+                            <li key={i}>{`${item.firstName} ${item.lastName}`}</li>
+                        ))}
+                        </ul>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        </Container>
+    )
+}
+
+const initialWorkers = []
+
+yup.setLocale({
+    mixed: {
+        required: 'Esto es un dato requerido'
+    }
+})
+
+const schemaValidation = yup.object({
+    startPoint: yup.string().required(),
+    finalPoint: yup.string().required(),
+    vehicleId: yup.string().required(),
+    // workers: yup.array().required(),
+    kilometers: yup.number().required(),
+    roundTrip: yup.string().required()
+})
+
+const mapStateToProps = state => ({
+    vehicles: state.vehicles
+})
+
+const mapDispatchToProps = dispatch => ({
+    onFetchVehicles: () => dispatch(actions.fetchVehicles())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewTravel);
---
+++ app/src/components/pages/Travels.js
@@ -0,0 +1,71 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import moment from 'moment';
+
+import * as actions from '../../store/actions';
+import { calcular_kgco2 } from '../../helpers/utils';
+
+import { Container } from 'semantic-ui-react';
+import { Table } from 'semantic-ui-react';
+
+
+function Travels(props) {
+
+    useEffect(() => {
+        console.log('[Travels] loaded!')
+        props.onFetchTravels()
+    }, [])
+
+    return (
+        <Container fluid>
+            <Table celled striped>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell textAlign='center'>#</Table.HeaderCell>
+                        <Table.HeaderCell textAlign='center'>dia y hora</Table.HeaderCell>
+                        <Table.HeaderCell textAlign='center'>punto de inicio</Table.HeaderCell>
+                        <Table.HeaderCell textAlign='center'>punto de termino</Table.HeaderCell>
+                        <Table.HeaderCell textAlign='center'>km recorridos</Table.HeaderCell>
+                        <Table.HeaderCell textAlign='center'>medio de transporte</Table.HeaderCell>
+                        <Table.HeaderCell textAlign='center'>personas en el viaje</Table.HeaderCell>
+                        <Table.HeaderCell textAlign='center'>ida y vuelta</Table.HeaderCell>
+                        <Table.HeaderCell textAlign='center'>Kg CO2</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                {props.travels && props.travels.map((item, i) => (
+                    <Table.Row key={item._id}>
+                        <Table.Cell textAlign='center'>{(i+1)}</Table.Cell>
+                        <Table.Cell textAlign='center'>{moment(item.created_at).format('DD/MM/YYYY')}</Table.Cell>
+                        <Table.Cell textAlign='center'>{item.startPoint}</Table.Cell>
+                        <Table.Cell textAlign='center'>{item.finalPoint}</Table.Cell>
+                        <Table.Cell textAlign='center'>{item.kilometers}</Table.Cell>
+                        <Table.Cell textAlign='center'>{item.vehicle.name}</Table.Cell>
+                        <Table.Cell textAlign='center'>{item.workers.length}</Table.Cell>
+                        <Table.Cell textAlign='center'>{item.roundTrip ? 'SI' : 'NO'}</Table.Cell>
+                        <Table.Cell textAlign='center'>
+                            {
+                                calcular_kgco2({ 
+                                    factor_emision: item.vehicle.emitfactor, 
+                                    kilometers: item.kilometers, 
+                                    roundTrip: item.roundTrip 
+                                })
+                            }
+                        </Table.Cell>
+                    </Table.Row>
+                ))}
+                </Table.Body>
+            </Table>
+        </Container>
+    )
+}
+
+const mapStateToProps = state => ({
+    travels: state.travels
+})
+
+const mapDispatchToProps = dispatch => ({
+    onFetchTravels: () => dispatch(actions.fetchTravels())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Travels);
